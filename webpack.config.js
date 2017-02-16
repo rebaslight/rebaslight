20a7283f56932a14813c54c58b0d69cc10be2552
Matthew Wright <matthew@smallhelm.com>
2017-02-16 10:02:36 -0600

    fixing process.env

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             "process.env": {
-                NODE_ENV: process.env.NODE_ENV
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
             }
         }),
 
