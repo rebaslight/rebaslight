a881b475a2a503f707a96560ec9b5ae338758b10
Matthew Wright <matthew@smallhelm.com>
2018-04-05 11:43:40 -0500

    upgrade deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,19 @@
 var path = require('path')
-var webpack = require('webpack')
-
-var node_env = process.env.NODE_ENV === 'development'
-  ? 'development'
-  : 'production'// default to prod
 
 var conf = {
+
+  mode: process.env.NODE_ENV === 'development'
+    ? 'development'
+    : 'production', // default to prod
+
   entry: './src/index.js',
-  devtool: 'source-map',
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'app')
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        use: [
-          {loader: 'eslint-loader'}
-        ],
-        exclude: /node_modules/
-      },
       {
         test: /\.css$/,
         use: [
@@ -34,32 +27,11 @@ var conf = {
           {loader: 'url-loader'}
         ]
       }
-    ],
-    noParse: /\.elm$/
+    ]
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(node_env)
-      }
-    })
-  ]
-}
-
-if (node_env === 'production') {
-  conf.devtool = 'source-map'
-
-  conf.plugins = conf.plugins.concat([
-    // Clean and minify JS bundle
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true
-    })
-  ])
-} else {
-  conf.devtool = 'eval'
+  performance: {
+    hints: false
+  }
 }
 
 module.exports = conf
